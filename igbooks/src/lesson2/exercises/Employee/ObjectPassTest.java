56924d96265bbfe11520e0bab3ef889526405528
RiaanJBredenhann <riaanbredenhann@gmail.com>
2024-02-05 16:12:13 +0200

    update

---
+++ igbooks/src/lesson2/exercises/Employee/ObjectPassTest.java
@@ -0,0 +1,17 @@
+package lesson2.exercises.Employee;
+
+public class ObjectPassTest {
+    public static void main(String[] args) {
+        ObjectPassTest test = new ObjectPassTest();
+        Employee x = new Employee ();
+        x.setSalary(120_000.00);
+        test.foo(x);
+        System.out.println ("Employee salary: " + x.getSalary());
+    }
+
+    public void foo(Employee e){
+        e.setSalary(130_000.00);
+        e = new Employee();
+        e.setSalary(140_000.00);
+    }
+}
