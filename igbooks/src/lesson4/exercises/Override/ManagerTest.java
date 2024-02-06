dff4d246154d146d28ecbf99aab14ab3d8cc0f30
RiaanJBredenhann <riaanbredenhann@gmail.com>
2024-02-06 09:57:29 +0200

    update

---
+++ igbooks/src/lesson4/exercises/Override/ManagerTest.java
@@ -0,0 +1,10 @@
+package lesson4.exercises.Override;
+
+public class ManagerTest {
+
+    public static void main(String args[]) {
+        Manager mgr = new Manager(151, "Tom", "444-44-4444", 8000, "admin");
+
+        System.out.println("Manager: " + mgr.getDetails());
+    }
+}
