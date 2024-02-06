3a42a3c72fb5672b3ae03412a3fe479f8f4ea841
RiaanJBredenhann <riaanbredenhann@gmail.com>
2024-02-06 10:22:23 +0200

    update

--- igbooks/src/lesson4/exercises/StaticExamples/A01MathTest.java
+++ igbooks/src/lesson4/exercises/StaticExamples/A01MathTest.java
@@ -1,11 +1,10 @@
 package lesson4.exercises.StaticExamples;
-
+import static java.lang.Math.*;
 public class A01MathTest {
     public static void main(String[] args) {
-        System.out.println("Random: " + Math.random() * 10);
-        System.out.println("Square root: " + Math.sqrt(9.0));
-        System.out.println("Rounded random: " +
-            Math.round(Math.random()*100));
-        System.out.println("Abs: " + Math.abs(-9));
+        System.out.println("Random: " + random() * 10);
+        System.out.println("Square root: " + sqrt(9.0));
+        System.out.println("Rounded random: " + round(random()*100));
+        System.out.println("Abs: " + abs(-9));
     }
 }
