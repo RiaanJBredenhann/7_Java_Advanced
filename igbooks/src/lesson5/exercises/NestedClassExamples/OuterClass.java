10a631526561b0374d468c42403c398bed99cdde
RiaanJBredenhann <riaanbredenhann@gmail.com>
2024-02-06 11:20:49 +0200

    update

---
+++ igbooks/src/lesson5/exercises/NestedClassExamples/OuterClass.java
@@ -0,0 +1,27 @@
+
+
+package lesson5.exercises.NestedClassExamples;
+
+
+public class OuterClass {
+     private int privInt = 10;
+ 
+  
+ public void createInnerClass() {
+
+ InnerClass inClass = new InnerClass();
+ inClass.accessOuter();
+ }
+ 
+
+ class InnerClass {  
+   
+  public void accessOuter() {
+
+  System.out.println("The outer class's privInt is " + privInt);
+
+  } 
+
+ }
+
+}
