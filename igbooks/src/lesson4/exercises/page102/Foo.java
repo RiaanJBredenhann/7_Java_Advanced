dd9a42c1252a17187dec59e358473ea5a7d6a209
RiaanJBredenhann <riaanbredenhann@gmail.com>
2024-02-06 09:57:14 +0200

    update

--- igbooks/src/lesson4/exercises/page102/Foo.java
+++
@@ -1,6 +0,0 @@
-package lesson4.exercises.page102;
-
-public class Foo {
-    protected int result = 20;
-    int num = 25;
-}
--- igbooks/src/lesson4/exercises/page103/Foo3.java
+++
@@ -1,8 +0,0 @@
-package lesson4.exercises.page103;
-
-public class Foo3 {
-    private int result = 20;
-    protected int getResult() {
-        return this.result;
-    }
-}
