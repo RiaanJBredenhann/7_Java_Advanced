3bbfaa70724296ecb1139dcbc368c414ede7b5a3
RiaanJBredenhann <riaanbredenhann@gmail.com>
2024-02-08 08:59:13 +0200

    update

--- igbooks/src/lesson8/exercises/RoboCallExample/RoboCallTest08.java
+++ igbooks/src/lesson8/exercises/RoboCallExample/RoboCallTest08.java
@@ -3,23 +3,18 @@ package lesson8.exercises.RoboCallExample;
 import java.util.List;
 import java.util.function.Predicate;
 
-/**
- *
- * @author MikeW
- */
-public class RoboCallTest08 {
-  
-  public static void main(String[] args){ 
+public class RoboCallTest08
+{
+    public static void main(String[] args)
+    {
+        List<Person> pl = Person.createShortList();
+        RoboCall05 robo = new RoboCall05();
+    
+        // Predicates
+        Predicate<Person> allPilots =
+            p -> p.getAge() >= 23 && p.getAge() <= 65;
 
-    List<Person> pl = Person.createShortList();
-    RoboCall05 robo = new RoboCall05();
-    
-    // Predicates
-    Predicate<Person> allPilots = 
-        p -> p.getAge() >= 23 && p.getAge() <= 65;
-    
-    System.out.println("\n=== Calling all Drivers Variable ===");
-    pl.stream().filter(allPilots)
-        .forEach(p -> robo.roboCall(p));       
-  }
+        System.out.println("\n=== Calling all Drivers Variable ===");
+        pl.stream().filter(allPilots).forEach(p -> robo.roboCall(p));
+    }
 }
