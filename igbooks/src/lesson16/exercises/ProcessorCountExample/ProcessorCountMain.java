8412980c40e99cf3fe263c8f6e74df2305576b89
RiaanJBredenhann <riaanbredenhann@gmail.com>
2024-02-21 09:11:29 +0200

    update

---
+++ igbooks/src/lesson16/exercises/ProcessorCountExample/ProcessorCountMain.java
@@ -0,0 +1,9 @@
+package lesson16.exercises.ProcessorCountExample;
+
+public class ProcessorCountMain {
+
+    public static void main(String[] args) {
+        int count = Runtime.getRuntime().availableProcessors();
+        System.out.println("Processor Count:" + count);
+    }
+}
