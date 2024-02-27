a25aec1f2daade1fecb104f24bace5635ba5d72f
RiaanJBredenhann <riaanbredenhann@gmail.com>
2024-02-27 10:06:33 +0200

    update

---
+++ igbooks/src/lesson19/exercises/DateFormatExample/Main.java
@@ -0,0 +1,9 @@
+package lesson19.exercises.DateFormatExample;
+
+public class Main {
+
+  public static void main(String[] args) {
+    DateFormatTest.main(args);
+  }
+  
+}
---
+++ igbooks/src/lesson19/exercises/PropertiesExample/Main.java
@@ -0,0 +1,8 @@
+package lesson19.exercises.PropertiesExample;
+
+public class Main {
+
+  public static void main(String[] args) {
+    PropertiesExample.main(args);
+  }
+}
